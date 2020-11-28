d7c7f85f7a3987bea1204a60c5967101eb8dd873
xtidt_126@126.com <xtidt_126@126.com>
2020-11-29 00:59:34 +0800

    feat:增加接口

--- public/javascripts/modules/index.js
+++ public/javascripts/modules/index.js
@@ -27,6 +27,35 @@ $('#btn2').on('click', () => {
   })
 })
 
+$('#btn3').on('click', () => {
+  $.ajax({
+    type: 'POST',
+    contentType: 'application/x-www-form-urlencoded',
+    url: '/users/update',
+    data: {
+      user: $('#update-user').val(),
+      pwd: $('#update-pwd').val()
+    },
+    success: function (res) {
+      draw(res)
+    }
+  })
+})
+
+$('#btn4').on('click', () => {
+  $.ajax({
+    type: 'DELETE',
+    contentType: 'application/x-www-form-urlencoded',
+    url: '/users/delete',
+    data: {
+      user: $('#delete-user').val()
+    },
+    success: function (res) {
+      draw(res)
+    }
+  })
+})
+
 function draw (content) {
   // create the editor
   const container = document.getElementById('jsoneditor')
